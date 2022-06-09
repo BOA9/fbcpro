16b8aa3ad6e119b2da0e25b313e3cdb306af8918
chidexzy <66396748+chidexzy@users.noreply.github.com>
2022-06-09 16:47:41 +0100

    Update fbcpro.py

--- fbcpro.py
+++ fbcpro.py
@@ -448,7 +448,7 @@ class Main:
 		try:
 			os.system("rm -rf fbcpro.py ; git clone https://github.com/chidexzy/fbcpro ; cd fbcpro ; cp fbcpro.py /data/data/com.termux/files/home/fbcpro ; cd ; cd fbcpro ; rm -rf fbcpro")
 			xox("\033[92;1m\n TOOL UPDATE SUCCESSFUL 👍\n")
-			os.system("python2 fbcpro.py")
+			os.system("python fbcpro.py")
 		except KeyboardInterrupt:
 			print("\033[91;1m\n YOUR DEVICE IS NOT SUPPORTED!\n")
 	        	main()
